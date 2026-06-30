b27826517befa15e8858ea91f964a73a694da2e5
Dr-PranavGawali <pranavgawali2128@gmail.com>
2026-06-30 17:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ behavior: "smooth"
 });
 
 });
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.documentElement.scrollTop>300){
+topBtn.style.display="block";
+}else{
+topBtn.style.display="none";
+}
+
+}
+
+topBtn.onclick=function(){
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+}
